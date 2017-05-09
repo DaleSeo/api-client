7e9413cd044807f409fb83f4eee1291d5815e851
Dale Seo <dale.seo@gmail.com>
2017-05-09 11:58:48 +0900

    favicon.ico

--- web/src/components/Navbar.vue
+++ web/src/components/Navbar.vue
@@ -4,7 +4,7 @@
       <b-nav-toggle target="nav_collapse"></b-nav-toggle>
       <b-link class="navbar-brand" to="#">
         <i class="fa fa-paper-plane"></i>
-        <strong>CHECK APIS</strong>
+        <strong>TRY APIS</strong>
         &nbsp;&nbsp;
         <small>Make API Calls Online</small>
       </b-link>
